265d76fc3bf978847a05f38428fe5dc3967ac5ef
Christophe Lafenetre <christophe.lafenetre@gmail.com>
2022-01-04 17:29:58 +0100

    seach bar ok avec pg seach et ajax

--- app/javascript/controllers/search_bullets_controller.js
+++ app/javascript/controllers/search_bullets_controller.js
@@ -3,16 +3,13 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ['form', 'input', 'list'];
 
-  connect(){
-    console.log(this.listTarget);
-  }
   update(event) {
-    console.log(this.listTarget);
+    event.preventDefault();
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
-        // this.listTarget.outerHTML = data;
+        this.listTarget.outerHTML = data;
       })
   }
 }
