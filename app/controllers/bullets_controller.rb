87573bfde4be0582c5166d9fcf2a0abfd8940e11
julien baret <julienco@gmail.com>
2022-01-04 16:15:39 +0100

    Commit des changements Ajax pour l'update d'un bullet

--- app/controllers/bullets_controller.rb
+++ app/controllers/bullets_controller.rb
@@ -1,22 +1,45 @@
 class BulletsController < ApplicationController
+  before_action :set_user
+
   def index
+    @new_bullet = Bullet.new
     if params["query"].present?
       @bullets = Bullet.search_by_content(params["query"])
     else
-      @bullets = Bullet.all
+      @bullets = Bullet.all.order(id: :asc)
+    end
+    respond_to do |format|
+    format.html # Follow regular flow of Rails
+    format.text { render partial: 'bullets/bullets', locals: { bullets: @bullets }, formats: [:html] }
     end
   end
 
   def create
-
+    @bullet = Bullet.new(bullet_params)
+    @bullet.user = @user
+    if @bullet.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   def update
-
+    @bullet = Bullet.find(params[:id])
+    @bullet.update(bullet_params)
   end
 
   def destroy
+  end
 
+private
+
+  def bullet_params
+    params.permit(:content, :person)
+  end
+
+  def set_user
+    @user = User.find(current_user[:id])
   end
 
 end
---
+++ app/javascript/controllers/bullet_list_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ['bullet', 'input', 'new'];
+
+  update(event) {
+    event.preventDefault();
+    const url = `${this.bulletTarget.action}`;
+    fetch(url, {
+      method: 'PATCH',
+      headers: { 'Accept': 'text/plain' },
+      body: new FormData(this.bulletTarget)})
+      .then(response => response.text())
+      .then((data) => {
+      })
+  }
+
+}
