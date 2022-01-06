0a157076dd6fd2e7b5ab0e289778100e4dee8cac
Christophe Lafenetre <christophe.lafenetre@gmail.com>
2022-01-06 14:54:20 +0100

    ok pour delete avec ctrl bacspace

--- app/javascript/controllers/bullet_list_controller.js
+++ app/javascript/controllers/bullet_list_controller.js
@@ -1,11 +1,17 @@
 import { Controller } from "stimulus";
+import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = ['bullet', 'input'];
+  static targets = ['bullet', 'input', "liBullet"];
 
   update(event) {
     event.preventDefault();
     const url = `${this.bulletTarget.action}`;
+    console.log(this.bulletTarget);
+    // Comment marche DATAFORM ??
+    // let toto = new FormData(this.bulletTarget)
+    // console.dir(toto)
+    // console.log(toto.values());
     fetch(url, {
       method: 'PATCH',
       headers: { 'Accept': 'text/plain' },
@@ -13,5 +19,24 @@ export default class extends Controller {
       .then(response => response.text())
       .then((data) => {
       })
+  }
+
+  destroy(event){
+    if (event.ctrlKey && event.key === "Backspace"){
+      // console.log('hello');
+      const url = this.bulletTarget.action
+      fetch(url, {
+        method: 'DELETE',
+        'Accept': "application/json",
+        // 'Content-Type': "application/json",
+        // 'X-CSRF-Token': csrfToken()
+      })
+      .then(response => {
+        // response.json()
+      })
+      .then((data)=> {
+      })
+      this.liBulletTarget.remove();
+    }
   }
 }
